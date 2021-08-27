249282fce1b64adbdb076755bd017e5f19e59405
SametCimen1 <cimensamet338@gmail.com>
2021-08-27 15:25:51 -0400

    try

--- index.js
+++ index.js
@@ -18,11 +18,13 @@ const path = require('path');
 //   app.use(express.static('userimg'))
 //   app.use(express.static("build"));
 // }
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static('./client/build'))
 // app.use(express.static('userimg'))
 // app.use(express.static("build"));
 
-
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+});
 
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/post');
@@ -259,13 +261,10 @@ app.post('/verify', async(req,res) =>{
   }
 
 })
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
-});
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-});
+
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`);
