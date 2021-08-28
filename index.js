7f1dfb4d92fa35b319f1cc8b5588719dad930078
SametCimen1 <cimensamet338@gmail.com>
2021-08-28 18:17:48 -0400

    upd

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "client/build")));
 app.use(express.static('./userimg/img'))
 app.use(express.static("./client/build"));
 
-// app.use(express.static('userimg'))
+app.use(express.static('userimg'))
 // app.use(express.static("build"));
 
 
