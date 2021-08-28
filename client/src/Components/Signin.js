c71a89fcefadcd1ecb8c3c701e8aa854958668dc
SametCimen1 <cimensamet338@gmail.com>
2021-08-28 18:53:22 -0400

    upt

--- client/src/Components/Signin.js
+++ client/src/Components/Signin.js
@@ -61,53 +61,54 @@ export default function Signup(){
 
 
   const onSuccess = async(res) => {
-    const password = await prompt('Choose password');
-    const email = res.profileObj.email;
-    const name = res.profileObj.givenName;
-    const img = res.profileObj.imageUrl;
+    console.log("SUCESSS")
+    // const password = await prompt('Choose password');
+    // const email = res.profileObj.email;
+    // const name = res.profileObj.givenName;
+    // const img = res.profileObj.imageUrl;
 
-      const data = await fetch('/api/user/checkemail', {
-      method:"POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({email:email})
-    });
-    const response = await data.json();
+    //   const data = await fetch('/api/user/checkemail', {
+    //   method:"POST",
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify({email:email})
+    // });
+    // const response = await data.json();
     
-    if(!response){ //doesnt exist
-      const data = await fetch('/api/user/signup', {
-        method:"POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({email:email,name:name,img:img,password:password})
-      });
-      const signin = await fetch('/api/user/signin', {
-        method:"POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        redirect: 'follow',
-        credentials: 'include',
-        body: JSON.stringify({email:email,password:password})
-      })
-      history.push("/");
-      history.go(0);
-    }
-    else{ //exist
-      const data = await fetch('/api/user/signin', {
-        method:"POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        redirect: 'follow',
-        credentials: 'include',
-        body: JSON.stringify({email:email,password:password})
-      })
-      history.push("/")
-      history.go(0);
-    };
+    // if(!response){ //doesnt exist
+    //   const data = await fetch('/api/user/signup', {
+    //     method:"POST",
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({email:email,name:name,img:img,password:password})
+    //   });
+    //   const signin = await fetch('/api/user/signin', {
+    //     method:"POST",
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     redirect: 'follow',
+    //     credentials: 'include',
+    //     body: JSON.stringify({email:email,password:password})
+    //   })
+    //   history.push("/");
+    //   history.go(0);
+    // }
+    // else{ //exist
+    //   const data = await fetch('/api/user/signin', {
+    //     method:"POST",
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     redirect: 'follow',
+    //     credentials: 'include',
+    //     body: JSON.stringify({email:email,password:password})
+    //   })
+    //   history.push("/")
+    //   history.go(0);
+    // };
 
     
 
